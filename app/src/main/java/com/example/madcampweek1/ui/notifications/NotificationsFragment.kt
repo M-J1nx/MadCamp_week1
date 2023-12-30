90e78d28e78cdb1e9db6e3f463e6a6c19b39b76f
M-J1nx <104704651+M-J1nx@users.noreply.github.com>
2023-12-30 17:57:09 +0900

    roulette is added

--- app/src/main/java/com/example/madcampweek1/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/madcampweek1/ui/notifications/NotificationsFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.madcampweek1.R
 import com.example.madcampweek1.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
@@ -28,10 +30,16 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+//        val textView: TextView = binding.textNotifications
+//        notificationsViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+
+        val rotateButton: Button = root.findViewById(R.id.rotateButton)
+        rotateButton.setOnClickListener {
+            rotateRoulette()
         }
+
         return root
     }
 
@@ -39,4 +47,21 @@ class NotificationsFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    fun rotateRoulette() {
+
+        val rotateListener = object: FoodRoulette.RotateListener {
+            override fun onRotateStart() {
+                binding.rotateResultTv.text = "오늘 먹을 음식의 카테고리는? : "
+            }
+
+            override fun onRotateEnd(result: String) {
+                binding.rotateResultTv.text = "오늘 먹을 음식의 카테고리는? : $result"
+            }
+        }
+
+        val toDegrees = (2000..10000).random().toFloat()
+        binding.roulette.rotateRoulette(toDegrees, 1000, rotateListener)
+    }
+
 }
